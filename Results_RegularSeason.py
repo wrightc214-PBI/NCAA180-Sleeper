dc66b0b3dd131d8ee7a3a07e28d9fff8f2b1f639
wrightc214-PBI <PowerBI@wrightc214.onmicrosoft.com>
2025-10-02 23:14:26 -0400

    Update Results_RegularSeason.py

--- Results_RegularSeason.py
+++ Results_RegularSeason.py
@@ -7,7 +7,6 @@ csv_path = os.path.join(script_dir, 'data', 'Matchups_AllYears.csv')
 
 matchups = pd.read_csv(csv_path)
 
-
 # Filter regular season (weeks 1-11)
 regular_season = matchups[(matchups['IsRegularSeason'] == True) & (matchups['Week'].between(1, 11))]
 regular_season = regular_season[~((regular_season['PointsFor'] == 0) & (regular_season['PointsAgainst'] == 0))]
@@ -18,16 +17,16 @@ regular_season['Loss'] = regular_season['PointsFor'] < regular_season['PointsAga
 regular_season['Tie'] = (regular_season['PointsFor'] == regular_season['PointsAgainst']) & (regular_season['PointsFor'] > 0)
 
 # Sort by Year, League, Week to prepare for cumulative metrics
-regular_season = regular_season.sort_values(['Year','LeagueID','Week','OwnerID'])
+regular_season = regular_season.sort_values(['Year','LeagueID','Week','OwnerID','RosterID'])
 
 # Cumulative team metrics by week
-weekly_metrics = regular_season.groupby(['Year','LeagueID','LeagueName','OwnerID','OwnerName','Week']).agg(
+weekly_metrics = regular_season.groupby(['Year','LeagueID','LeagueName','OwnerID','OwnerName','RosterID','Week']).agg(
     Wins=('Win','sum'),
     Losses=('Loss','sum'),
     Ties=('Tie','sum'),
     PointsFor=('PointsFor','sum'),
     PointsAgainst=('PointsAgainst','sum')
-).groupby(level=[0,1,2,3,4]).cumsum().reset_index()
+).groupby(level=[0,1,2,3,4,5]).cumsum().reset_index()
 
 # Games played and averages
 weekly_metrics['GamesPlayed'] = weekly_metrics['Wins'] + weekly_metrics['Losses'] + weekly_metrics['Ties']
@@ -47,4 +46,4 @@ weekly_metrics = weekly_metrics.sort_values(['Year','LeagueName','Week','Playoff
 # Save for Power BI
 weekly_metrics.to_csv('data/Results_RegularSeason.csv', index=False)
 
-print("Weekly metrics with Playoff rank calculated and saved to WeeklyMetrics_RegularSeason.csv")
+print("Weekly metrics with Playoff rank calculated and saved to Results_RegularSeason.csv")
