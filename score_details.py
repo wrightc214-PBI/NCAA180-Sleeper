4a3b572829f0bcde35bd74cfdac7332f5dca1a6f
wrightc214-PBI <PowerBI@wrightc214.onmicrosoft.com>
2026-09-22 22:30:55 -0400

    Update score_details.py

--- score_details.py
+++ score_details.py
@@ -192,7 +192,7 @@ if current_week is None:
 
 if current_week is not None:
     this_week_df = combined_df[combined_df["weekNum"].astype(int) == int(current_week)]
-    this_week_df.to_csv("data/Scores_ThisWeek.csv", index=False)
+    this_week_df.to_csv("data/Scores_Week.csv", index=False)
     print(f"📄 Saved {len(this_week_df)} rows for week {current_week} to data/Scores_ThisWeek.csv")
 
 # -------------------------
